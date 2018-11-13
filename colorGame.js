34ad887c908bd7448ae55f7c34d7c88d96c4c303
Emmanuel Ikehi <emmanuel.ikehi@gamesys.co.uk>
2018-11-13 17:40:00 +0000

    change squares color to the correct color

--- colorGame.js
+++ colorGame.js
@@ -21,13 +21,20 @@ for (var i = 0; i < squares.length; i++){
     //add click listeners to squares
     squares[i].addEventListener("click", function(){
         //grab color of the clicked square
-        var clickedSquare = this.style.backgroundColor;
+        var clickedcolor = this.style.backgroundColor;
         //compare color to picked color
-        if (pickedColor === clickedSquare) {
+        if (pickedColor === clickedcolor) {
             message.textContent = "Correct!";
+            changeColor(clickedcolor);
         } else {
             this.style.backgroundColor = "#232323";
             message.textContent = "Try Again!";
         }
     })
+};
+
+function changeColor(color){
+    for (var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
 }
